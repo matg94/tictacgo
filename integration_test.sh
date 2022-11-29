38f56798638bbc2c2f556184f5ef2ed7b2e6420a
mat gautron <mat.gautron@gmail.com>
2022-11-29 12:20:08 +0000

    Cleanup test script

--- integration_test.sh
+++ integration_test.sh
@@ -1,18 +1,21 @@
-ID=$(curl --silent localhost:8080/createLobby -d '{"playerId": "mat"}' | jq -r .'lobbyId')
+URL=http://localhost
+
+
+ID=$(curl --silent ${URL}:8080/createLobby -d '{"playerId": "mat"}' | jq -r .'lobbyId')
 echo Lobby ID: ${ID}
-NEXT=$(curl --silent localhost:8080/joinLobby/${ID} -d '{"playerId": "pete"}' | jq -r .'board'.'nextToPlay')
+NEXT=$(curl --silent ${URL}:8080/joinLobby/${ID} -d '{"playerId": "pete"}' | jq -r .'board'.'nextToPlay')
 echo Next To Play: ${NEXT}
 echo Pete tries to play when it is not his turn...
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 4}' | jq .'error'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 4}' | jq .'error'
 echo Mat plays middle
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 4}' | jq .'board'.'tiles'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 4}' | jq .'board'.'tiles'
 echo Pete tries to play middle as well
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 4}' | jq .'error'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 4}' | jq .'error'
 echo Pete plays top middle
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 2}' | jq .'board'.'tiles'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 2}' | jq .'board'.'tiles'
 echo Mat plays middle right
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 5}' | jq .'board'.'tiles'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 5}' | jq .'board'.'tiles'
 echo Pete plays bottom right 
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 8}' | jq .'board'.'tiles'
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "pete", "tileLocation": 8}' | jq .'board'.'tiles'
 echo Mat plays middle left
-curl --silent localhost:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 3}' | jq
+curl --silent ${URL}:8080/makeMove/${ID} -d '{"playerId": "mat", "tileLocation": 3}' | jq
