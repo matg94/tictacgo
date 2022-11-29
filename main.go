2596815b6a1ded7d853ec1a1717ddf17c85d8a46
mat gautron <mat.gautron@gmail.com>
2022-11-29 10:29:01 +0000

    Added hello endpoint for tests

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Hello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello, world!",
+	})
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/hello", Hello)
+	err := router.Run()
+	if err != nil {
+		log.Fatalf("failed to start server: %s", err)
+	}
+
 }
